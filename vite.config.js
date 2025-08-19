bbab20c246e301cb891f0a264e2c0fbb477efca1
Tarun <tarunuday512@gmail.com>
2025-08-19 18:31:43 +0530

    /* /index.html 200

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // If deploying to GitHub Pages at /<repo>/, set `base: '/<repo>/'`.
 export default defineConfig({
   plugins: [react()],
-  // base: '/portfolio/',
+  base: './',
 })
