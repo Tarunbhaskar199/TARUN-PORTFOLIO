47b31bf2f517b46b52714d167958f8620e86ef69
Tarun <tarunuday512@gmail.com>
2025-09-11 16:43:09 +0530

    projects.data.js

--- src/projects.data.js
+++ src/projects.data.js
@@ -3,24 +3,30 @@ export const projects = [
     title: "Crop Recommender",
     desc: "Recommends best crops based on soil and climate data.",
     tech: ["Python", "Pandas", "ML"],
-    link: "https://github.com/Tarunbhaskar199"
+    link: "https://github.com/Tarunbhaskar199/Crop-Recommender-System"
   },
   {
     title: "Notetaker",
     desc: "A simple note-taking web app built with HTML, CSS, and JavaScript.",
     tech: ["HTML", "CSS", "JS"],
-    link: "https://github.com/Tarunbhaskar199"
+    link: "https://github.com/Tarunbhaskar199/notetaker"
   },
   {
     title: "Personal Portfolio",
     desc: "This is my personal portfolio website, built with HTML, CSS (Tailwind).",
     tech: ["HTML", "CSS","React.js"],
-    link: "https://github.com/Tarunbhaskar199"
+    link: "https://github.com/Tarunbhaskar199/TARUN-PORTFOLIO"
   },
   {
     title: "Student Management",
     desc: "A simple Java console application to manage student records with MySQL integration.",
     tech: ["Java", "SQL"],
-    link: "https://github.com/Tarunbhaskar199"
+    link: "https://github.com/Tarunbhaskar199/Student-Management"
+  },
+  {
+    title: "E-Commerce Dashboard",
+    desc: "Power BI dashboard analyzing customer orders by state, city, and time using Kaggle dataset.",
+    tech: ["Excel", "python", "PowerBI"],
+    link: "https://github.com/Tarunbhaskar199/E-Commerce-Dashboard"
   }
 ]
